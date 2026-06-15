9fba3ae53fde4a359fa54cc5ff7614bd2b4860c3
npub1t89vhkp66hz54kga4n635jwqdc977uc2crnuyddx7maznwfrpupqwra5h9 <129702033+ColbySerpa@users.noreply.github.com>
2026-06-15 01:35:26 -0700

    Fix hosted preview asset loading
    - Serve KaTeX fonts from the built dist/fonts path used by GitHub Pages
    
    - Put the component HTML in standards mode for KaTeX rendering
    
    - Rebuild the published dist assets after the source fix

--- com.sncommunity.markdown-basic/webpack.config.js
+++ com.sncommunity.markdown-basic/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../',
+              publicPath: './',
             },
           },
           'css-loader',
